b2c18df33a12fd3544dfd8734deef0a56d671486
Lucanamon <holy_agnus@hotmail.com>
2026-03-06 16:57:44 +0700

    running flow: safe initial step for continuity process

--- backend/Bestivale.API/Program.cs
+++ backend/Bestivale.API/Program.cs
@@ -2,29 +2,41 @@ using Bestivale.Application.Interfaces;
 using Bestivale.Infrastructure.Data;
 using Bestivale.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Bestivale API",
+        Version = "v1",
+        Description = "Bestivale Monster Encyclopedia API"
+    });
+});
+
+// Database
 builder.Services.AddDbContext<BestivaleDbContext>(options =>
     options.UseInMemoryDatabase("Bestivale"));
 
+// Services
 builder.Services.AddScoped<IMonsterService, MonsterService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline
+app.UseSwagger();
+
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Bestivale API v1");
+    options.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
 
--- backend/Bestivale.API/obj/Debug/net8.0/Bestivale.API.AssemblyInfo.cs
+++ backend/Bestivale.API/obj/Debug/net8.0/Bestivale.API.AssemblyInfo.cs
@@ -11,11 +11,11 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Bestivale")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Bestivale.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Bestivale")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1255e58d9e1ae35bd5241448378aaa94dc7c151c")]
+[assembly: System.Reflection.AssemblyProductAttribute("Bestivale.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bestivale.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
